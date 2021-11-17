0700686aa5fac9c5d9a42b59a02e03728c8ec55b
ooops4 <ooops4@gmail.com>
2021-11-17 11:52:57 +0530

    folder create by id and zip save by id in folder

--- src/controller/file.controller.js
+++ src/controller/file.controller.js
@@ -5,6 +5,7 @@ const baseUrl = "http://localhost:8080/files/";
 const upload = async (req, res) => {
   try {
     await uploadFile(req, res);
+    
 
     if (req.file == undefined) {
       return res.status(400).send({ message: "Please upload a file!" });
