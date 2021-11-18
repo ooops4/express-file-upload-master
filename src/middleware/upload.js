37f498dd025eb568f851cf71955f8d2df5e5a0c0
ooops4 <ooops4@gmail.com>
2021-11-19 02:59:28 +0530

    upload->convert and download via queueId working

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -2,9 +2,10 @@ const util = require("util");
 const multer = require("multer");
 const fs = require("fs");
 const path = require("path");
-const maxSize = 2 * 1024 * 1024;
+const maxSize = 2 * 1024 * 1024 * 1024;
 let pdfFolder = "";
 const AppData = "AppData";
+const HtmlOutputDirectory = "HTMLOutput";
 
 
 let storage =
@@ -19,9 +20,20 @@ let storage =
         fs.mkdirSync(path.join(__basedir, AppData));
       }
 
+      if (!fs.existsSync(path.join(__basedir, HtmlOutputDirectory))) {
+        fs.mkdirSync(path.join(__basedir, HtmlOutputDirectory));
+        // fs.chmodSync(path.join(__basedir, HtmlOutputDirectory), 0777);
+      }
+
       if (!fs.existsSync(path.join(__basedir, AppData, pdfFolder))) {
         fs.mkdirSync(path.join(__basedir, AppData, pdfFolder));
       }
+
+      if (!fs.existsSync(path.join(__basedir, HtmlOutputDirectory, pdfFolder))) {
+        fs.mkdirSync(path.join(__basedir, HtmlOutputDirectory, pdfFolder));
+        // fs.chmodSync(path.join(__basedir, HtmlOutputDirectory, pdfFolder), 0777);;
+      }
+
       cb(null, path.join(__basedir, AppData, pdfFolder));
     },
     filename: (req, file, cb) => {
