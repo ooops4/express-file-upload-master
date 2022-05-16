b997808b5a5357211ea06844869f261394632d1e
Bikram Prasad <ooops4@gmail.com>
2022-05-16 21:53:32 +0530

    endpoint commented

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const controller = require("../controller/file.controller");
 
 let routes = (app) => {
   router.post("/upload", controller.upload);
-  router.get("/files", controller.getListFiles);
+  // router.get("/files", controller.getListFiles);
   router.get("/files/:name", controller.download);
 
   app.use(router);
